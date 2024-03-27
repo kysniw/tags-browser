314ddb19bb212dee308641489591173178d6ca27
kysniw <kuba.winsyk.forgames@gmail.com>
2024-03-27 16:07:09 +0100

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <div
+      className="text-3xl font-bold min-h-screen flex items-center justify-center
+     bg-gradient-to-r from-cyan-500 to-blue-500"
+    >
+      Hello world!
+    </div>
+  );
+}
+
+export default App;
